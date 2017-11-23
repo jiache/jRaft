b91f502937eb23e816e55d6563f3c114ca14e2d1
jiache <jiacheng.huang@outlook.com>
2017-11-23 14:31:10 +0800

    update

---
+++ offload-server/src/main/java/io/jiache/offloadServer/BaseServer.java
@@ -0,0 +1,138 @@
+package io.jiache.offloadServer;
+
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.jiache.common.*;
+import io.jiache.grpc.offload.*;
+import io.jiache.util.Serializer;
+import org.rocksdb.RocksDBException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.LongStream;
+
+abstract public class BaseServer extends ServerServiceGrpc.ServerServiceImplBase implements Server {
+    protected Integer term;
+    protected volatile Long lastCommitIndex;
+    protected Log log;
+    protected StateMachine stateMachine;
+    protected ExecutorService executorService;
+    protected RaftConf raftConf;
+    protected Integer thisIndex;
+    protected volatile Long[] nextIndex;
+    protected volatile Lock[] raftLocks;
+
+    protected BaseServer(RaftConf raftConf, int thisIndex) {
+        try {
+            term = 0;
+            lastCommitIndex = -1L;
+            log = new DefaultLog(raftConf.getToken()+"-"+ UUID.randomUUID().toString());
+            stateMachine = new DefaultStateMachine();
+            executorService = Executors.newCachedThreadPool();
+            executorService.submit(()-> {
+                try {
+                    io.grpc.Server server = ServerBuilder.forPort(raftConf.getAddressList().get(thisIndex).getPort())
+                            .addService(this)
+                            .build()
+                            .start();
+                    Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+                    server.awaitTermination();
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            this.raftConf = raftConf;
+            this.thisIndex = thisIndex;
+            nextIndex = new Long[raftConf.getAddressList().size()];
+            for(int i=0; i<raftConf.getAddressList().size(); ++i) {
+                nextIndex[i] = 0L;
+            }
+            raftLocks = new ReentrantLock[raftConf.getAddressList().size()];
+            for(int i=0; i<raftLocks.length; ++i) {
+                raftLocks[i] = new ReentrantLock();
+            }
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void commit(long toIndex) {
+        long[] logIndex = LongStream.range(lastCommitIndex + 1, toIndex + 1).toArray();
+        Entry[] entries = log.get(logIndex);
+        stateMachine.commit(entries);
+        lastCommitIndex = toIndex;
+    }
+
+
+    public String get(String key) {
+        return stateMachine.get(key);
+    }
+
+    @Override
+    public void appendEntries(AppendEntriesRequest request, StreamObserver<AppendEntriesResponse> responseObserver) {
+        int term0 = request.getTerm();
+        AppendEntriesResponse.Builder responseBuilder = AppendEntriesResponse.newBuilder();
+        if(term0<term) {
+            responseObserver.onCompleted();
+            responseBuilder.setTerm(term)
+                    .setSuccess(false);
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+            return;
+        }
+        term = term0;
+        int preLogIndex0 = request.getPreLogIndex();
+        Entry entry0 = Serializer.deSerialize(request.getEntry().getBytes(), Entry.class);
+        if(!log.match(entry0.getTerm(), preLogIndex0)) {
+            responseBuilder.setSuccess(false)
+                    .setTerm(term);
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+            return;
+        }
+        log.append(entry0);
+        responseBuilder.setTerm(term)
+                .setSuccess(true);
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void callBack(CallBackRequest request, StreamObserver<CallBackResponse> responseObserver) {
+        CallBackResponse.Builder responseBuilder = CallBackResponse.newBuilder();
+        long term0 = request.getTerm();
+        if(term0 == term && request.getServerIndex()==raftConf.getLeaderIndex()){
+            CallBackResponse response = responseBuilder.setTerm(term)
+                    .setSuccess(true)
+                    .setReplicatedIndex((int) log.getLastIndex())
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            // commit
+            long lastCommitIndex0 = request.getLatestCommitIndex();
+            lastCommitIndex0 = Math.min(lastCommitIndex0, log.getLastIndex());
+            if(lastCommitIndex0>lastCommitIndex) {
+                commit(lastCommitIndex0);
+            }
+            return;
+        } else if(term0 > term) {  // TODO detective new leader
+            term = (int) term0;
+            raftConf.setLeaderIndex(request.getServerIndex());
+            responseBuilder.setSuccess(true);
+            responseBuilder.setTerm(term);
+        } else {  // out of date
+            responseBuilder.setSuccess(false);
+            responseBuilder.setTerm(term);
+        }
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+}
---
+++ original-server/src/main/java/io/jiache/originalServer/Leader.java
@@ -0,0 +1,115 @@
+package io.jiache.originalServer;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.jiache.common.*;
+import io.jiache.grpc.original.AppendEntriesRequest;
+import io.jiache.grpc.original.AppendEntriesResponse;
+import io.jiache.grpc.original.ServerServiceGrpc;
+import io.jiache.util.Serializer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leader extends BaseServer{
+    private List<ServerServiceGrpc.ServerServiceBlockingStub> blockingStubList;
+
+    public Leader(RaftConf raftConf, int thisIndex) {
+        super(raftConf, thisIndex);
+        blockingStubList = new ArrayList<>();
+        List<Address> addresses = raftConf.getAddressList();
+        for(int i=0; i<addresses.size(); ++i) {
+            ServerServiceGrpc.ServerServiceBlockingStub blockingStub = null;
+            if(i!=thisIndex) {
+                ManagedChannel managedChannel = ManagedChannelBuilder
+                        .forAddress(addresses.get(i).getHost(), addresses.get(i).getPort())
+                        .usePlaintext(true)
+                        .build();
+                blockingStub = ServerServiceGrpc.newBlockingStub(managedChannel);
+            }
+            blockingStubList.add(blockingStub);
+        }
+    }
+
+    // put操作 只有leader有 不需要logMatch
+    public void put(String key, String value) {
+        put(new Entry(key, value, term));
+    }
+
+    public void put(Entry entry) {
+        long index = log.append(entry);
+//        while(lastCommitIndex<index) {
+//            try {
+//                Thread.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
+    public void put(Entry[] entries) {
+        Arrays.stream(entries).forEach(this::put);
+    }
+
+    private void matainCommit() {
+        // 扫描nextIndex, 执行commit
+        Long[] sortedNextIndex;
+        for(;;) {
+            sortedNextIndex = Arrays.copyOfRange(nextIndex, 0, nextIndex.length);
+            Arrays.sort(sortedNextIndex);
+            long newCommitIndex = sortedNextIndex[sortedNextIndex.length/2+1]-1;
+            if(newCommitIndex > lastCommitIndex) {
+                commit(newCommitIndex);
+            }
+        }
+    }
+
+    private void appendEntriesToFollower(int followerIndex) {
+        if(raftLocks[followerIndex].tryLock()) {
+            Entry entry;
+            long lastIndex = log.getLastIndex();
+            if(nextIndex[followerIndex]<=lastIndex) {
+                entry = log.get(nextIndex[followerIndex]);
+            } else {
+                entry = new Entry(null, null, term);
+            }
+            AppendEntriesRequest request = AppendEntriesRequest.newBuilder()
+                    .setCommittedIndex(lastCommitIndex.intValue())
+                    .setTerm(term)
+                    .setEntry(new String(Serializer.serialize(entry)))
+                    .setPreLogIndex(nextIndex[followerIndex].intValue()-1)
+                    .build();
+            AppendEntriesResponse response = blockingStubList.get(followerIndex).appendEntries(request);
+            if(response.getSuccess() && entry.getKey()!=null) {
+                ++nextIndex[followerIndex];
+            } else if(term < response.getTerm()) {
+                term = response.getTerm(); // TODO become follower
+            }
+            raftLocks[followerIndex].unlock();
+        }
+    }
+
+    private void appendEntriesToFollowers() {
+        for(;;) {
+            for (int i = 0; i < raftConf.getAddressList().size(); ++i) {
+                if (i != thisIndex) {
+                    int finalI = i;
+                    executorService.submit(()->appendEntriesToFollower(finalI));
+                }
+            }
+            try {
+                Thread.sleep(HEART_BEAT_INTERVAL);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void run() {
+        executorService.submit(this::appendEntriesToFollowers);
+        executorService.submit(this::matainCommit);
+    }
+}
